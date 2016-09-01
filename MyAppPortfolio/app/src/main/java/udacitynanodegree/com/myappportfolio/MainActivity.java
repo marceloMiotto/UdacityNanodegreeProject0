fc2d13f067755bbee64f2ec412325dd74381c844
Marcelo_Miotto <mmiotto@gmail.com>
2016-09-01 14:39:03 -0300

    Fixes after Code Review from Udacity

--- MyAppPortfolio/app/src/main/java/udacitynanodegree/com/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/udacitynanodegree/com/myappportfolio/MainActivity.java
@@ -1,12 +1,16 @@
 package udacitynanodegree.com.myappportfolio;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,39 +18,19 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
     }
 
-    private void showToast(String msg){
-        Toast.makeText(this,"This button will launch my "+msg+" app!",Toast.LENGTH_SHORT).show();
-    }
-
-    public void getPortfolioApp(View v){
-
-        switch (v.getId()){
-
-            case R.id.button_popular_movies_project:
-                showToast(getString(R.string.popular_movies_project));
-                break;
-
-            case R.id.button_stock_hawk_project:
-                showToast(getString(R.string.stock_hawk_project));
-                break;
-
-            case R.id.button_build_it_bigger_project:
-                showToast(getString(R.string.build_it_bigger_project));
-                break;
-
-            case R.id.button_make_app_material_project:
-                showToast(getString(R.string.make_app_material_project));
-                break;
-
-            case R.id.button_go_ubiquitous_project:
-                showToast(getString(R.string.go_ubiquitous_project));
-                break;
-
-            case R.id.button_capstone_project:
-                showToast(getString(R.string.capstone_project));
-                break;
-
+    private void showToast(Button btn){
+        if (toast != null) {
+            toast.cancel();
         }
-
+        String msg    = String.format(getString(R.string.button_action_message),btn.getText().toString());
+        toast = Toast.makeText(this,msg,Toast.LENGTH_SHORT);
+        toast.show();
     }
+
+    @Override
+    public void onClick(View v) {
+        showToast((Button) v);
+    }
+
+
 }
